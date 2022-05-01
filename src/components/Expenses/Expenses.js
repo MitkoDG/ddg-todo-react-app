cab30731ffe6f9cc084349c61d11b00aaa96e404
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-01 22:38:29 +0300

    adding logic to the filter so it can work

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,20 +8,33 @@ import ExpensesFilter from './ExpensesFilter';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2022')
 
+  const filteredExpenses = props.items.filter(i => {
+    return i.date.getFullYear().toString() === filteredYear;
+  })
+
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
+  };
+
+  let expensesContent = <p>No expenses found !</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date} />
+    ))
   };
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map(expense => (
-        <ExpenseItem 
-        title={expense.title} 
-        amount={expense.amount} 
-        date={expense.date}/>
-        ))}
+
+        { expensesContent }
+
       </Card>
     </div>
   );
