4d76598f4e97d8fbfd63a4b334f7faf427dded87
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-02 08:59:40 +0300

    add chartbar

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -57,32 +57,30 @@ const ExpenseForm = (props) => {
         setEnteredDate('');
 
     };
-    const switchDisplay = (event) =>{
-        
-    }
+
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
-                <div className="new-expense__control" style={{display:"none"}}>
+                <div className="new-expense__control">
                     <label>Title</label>
                     <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
-                <div className="new-expense__control" style={{display:"none"}}>
+                <div className="new-expense__control">
                     <label>Amount</label>
                     <input type='number' value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
-                <div className="new-expense__control" style={{display:"none"}}>
+                <div className="new-expense__control">
                     <label>Date</label>
                     <input type='date' value={enteredDate} min="2019-01-01" step="2050-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
-            <div className="new-expense__actions" style={{display:"none"}}>
+            <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
-            <div className="new-expense__actions" style={{display:"block"}}>
-                <button onClick={switchDisplay}>Add New Expense</button>
+            <div className="new-expense__actions">
             </div>
-            
+
         </form>
     );
 };
