30371466b899d2f2f0e81c5f4ba6ecdaa35e1ad6
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-02 10:53:35 +0300

    fine modifications

--- src/App.js
+++ src/App.js
@@ -8,25 +8,37 @@ const DUMMY_EXPENSES = [
     id: 'e1',
     title: 'Toilet Paper',
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2022, 7, 14),
   },
   {
     id: 'e2',
     title: 'New TV',
     amount: 799.49,
-    date: new Date(2021, 2, 12)
+    date: new Date(2022, 2, 12)
   },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 2, 28),
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2022, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Dron',
+    amount: 450,
+    date: new Date(2021, 4, 13),
+  },
+  {
+    id: 'e5',
+    title: 'Dron',
+    amount: 450,
+    date: new Date(2020, 2, 22),
   }
 ];
 
@@ -46,7 +58,7 @@ const App = () => {
   //   {},
   //   React.createElement('h2', {}, "Let's get started!"),
   //   React.createElement(Expenses, { items: expenses })
-  // );
+  // ); // this is under the hood
 
   return (
     <div>
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>€{props.amount}</div>
       </div>
     </Card>
     </li>
